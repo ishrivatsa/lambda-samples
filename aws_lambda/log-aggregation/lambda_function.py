191adaa5fc5e41735efbd2d54d9a83b9cd513529
Shrivatsa Upadhye <ushrivatsa@gmail.com>
2019-10-21 11:01:59 -0700

    Add comments

--- aws_lambda/log-aggregation/lambda_function.py
+++ aws_lambda/log-aggregation/lambda_function.py
@@ -4,7 +4,9 @@ import io
 import json
 import zlib
 
-def cloudwatch_handler(event, context):
+## This function will process events/data sent to Kinesis Firehose. These events were aggregated in CloudWatch Logs. CloudWatch logs compresses the logs to reduce the size of data sent. This lambda function will first unzip/uncompress the zipped file. Then it looks for the type of the message/log. In general, there are 2 types of messages: CONTROL_MESSAGE and DATA_MESSAGE. CONTROL_MESSAGE(s) are used for communication between services whereas DATA_MESSAGE(s) are used to store log and event info. In this example, we are dropping the CONTROL_MESSAGE and just forwarding DATA_MESSAGE . 
+
+def lambda_handler(event, context):
     output = []
 
     for record in event['records']:
@@ -23,7 +25,9 @@ def cloudwatch_handler(event, context):
           return {'records': output}
      
      
-       # Do custom processing on the payload here
+       # Do custom processing on the payload here, if needed. 
+    
+     # Kinesis Firehose expects a particular format for the messages. Make sure the section below is NOT modified. 
       output_record = {
           'recordId': record['recordId'],
           'result': 'Ok',
